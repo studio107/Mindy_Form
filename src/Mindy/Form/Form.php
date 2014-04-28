7db0cb691f0d6b0fc1032f2480c79de60b7a4148
Falaleev Maxim <max@studio107.ru>
2014-04-28 11:59:52 +0400

    Add Form class for backward compatability

---
+++ src/Mindy/Form/Form.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 28/04/14.04.2014 11:59
+ */
+
+namespace Mindy\Form;
+
+
+class Form extends BaseForm
+{
+
+}

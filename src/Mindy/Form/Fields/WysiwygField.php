7ed7d0bddd999cbb34a9d51130913416fd359351
Falaleev Maxim <max@studio107.ru>
2014-06-07 18:52:29 +0400

    Up wysiwyg field

---
+++ src/Mindy/Form/Fields/WysiwygField.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 15/05/14.05.2014 18:11
+ */
+
+namespace Mindy\Form\Fields;
+
+use Yii;
+use CJavaScript;
+
+class WysiwygField extends TextAreaField
+{
+    public $options = [
+        'airMode' => false,
+        'minHeight' => 200
+    ];
+
+    public function init()
+    {
+        parent::init();
+        if($this->html === null) {
+            $this->html = [];
+        }
+        if(is_array($this->html)) {
+            if(!array_key_exists('class', $this->html)) {
+                $this->html['class'] = '';
+            }
+            $this->html['class'] .= ' wysiwyg';
+        }
+    }
+
+    public function render()
+    {
+        if(!isset($this->options['flowOptions'])) {
+            $http = Yii::app()->request;
+            $this->options['flowOptions'] = [
+                'target' => Yii::app()->urlManager->createUrl('files.upload'),
+                'chunkSize' => 1024 * 1024,
+                'testChunks' => false,
+                'query' => [$http->csrfTokenName => $http->getCsrfToken()]
+            ];
+        }
+        $options = CJavaScript::encode($this->options);
+
+        if(isset($this->options['airMode']) && $this->options['airMode']) {
+            $this->template = "<section id='{id}' name='{name}'{html}>{value}</section>";
+        }
+
+        $js = "<script type='text/javascript'>$('.wysiwyg').summernote({$options});</script>";
+        return parent::render() . $js;
+    }
+}

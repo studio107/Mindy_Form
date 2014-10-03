d390bc41ad54e9d58db5351836a8289d52b14cf4
Falaleev Maxim <max@studio107.ru>
2014-10-03 17:03:06 +0400

    Up

--- src/Mindy/Form/Fields/WysiwygField.php
+++ src/Mindy/Form/Fields/WysiwygField.php
@@ -14,7 +14,21 @@
 
 namespace Mindy\Form\Fields;
 
+use Mindy\Helper\JavaScript;
+
 class WysiwygField extends TinymceField
 {
-
+//    public function render()
+//    {
+//        $model = $this->form->getInstance();
+//        $options = Javascript::encode([
+//            'language' => 'ru',
+//            'plugins' => ['space', 'text', 'image', 'video'],
+//            'image' => [
+//                'uploadUrl' => '/core/files/upload/?path=' . $model->getModuleName() . '/' . $model->classNameShort()
+//            ]
+//        ]);
+//        $js = "<script type='text/javascript'>var editor = meditor.init('#{$this->getId()}', $options);</script>";
+//        return parent::render() . $js;
+//    }
 }

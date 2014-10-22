0228946f3304f67e3b4025ef2a081c46d5231b5e
Falaleev Maxim <max@studio107.ru>
2014-10-22 18:18:34 +0400

    Remove mysql database from tests

--- Tests/Cases/Form/ModelFormTest.php
+++ Tests/Cases/Form/ModelFormTest.php
@@ -121,13 +121,6 @@ class ModelFormTest extends TestCase
             'databases' => [
                 'default' => [
                     'class' => '\Mindy\Query\Connection',
-                    'dsn' => 'mysql:host=localhost;dbname=tmp',
-                    'username' => 'root',
-                    'password' => '123456',
-                    'charset' => 'utf8',
-                ],
-                'sqlite' => [
-                    'class' => '\Mindy\Query\Connection',
                     'dsn' => 'sqlite::memory:',
                 ]
             ]

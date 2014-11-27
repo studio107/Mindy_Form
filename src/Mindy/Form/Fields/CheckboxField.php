1dabac9a11e62e53ae5e0e10c71eaf4f0c66087c
antonokulov <qantus@mail.ru>
2014-11-27 11:22:06 +0300

    Checkbox choices added

--- src/Mindy/Form/Fields/CheckboxField.php
+++ src/Mindy/Form/Fields/CheckboxField.php
@@ -23,23 +23,49 @@ class CheckboxField extends CharField
 
     public function render()
     {
-        $label = $this->renderLabel();
-        if($this->value) {
-            $this->html['checked'] = 'checked';
-        }
-        $input = strtr($this->template, [
-            '{type}' => $this->type,
-            '{id}' => $this->getHtmlId(),
-            '{name}' => $this->getHtmlName(),
-            '{value}' => 1,
-            '{html}' => $this->getHtmlAttributes()
-        ]);
+        if (!empty($this->choices)) {
+            $inputs = [];
+            $i = 0;
+            foreach ($this->choices as $value => $labelStr) {
+                $label = strtr("<label for='{for}'>{label}</label>", [
+                    '{for}' => $this->getHtmlId() . '_' . $i,
+                    '{label}' => $labelStr
+                ]);
+                $input = strtr($this->template, [
+                    '{type}' => $this->type,
+                    '{id}' => $this->getHtmlId() . '_' . $i,
+                    '{name}' => $this->getHtmlName() . '[]',
+                    '{value}' => $value,
+                    '{html}' => $this->getHtmlAttributes()
+                ]);
+                $i++;
+                $inputs[] = implode("\n", [$input, $label]);
+            }
+            return implode("\n", [
+                "<input type='hidden' value='' name='" . $this->getHtmlName() . '[]' . "' />",
+                implode("\n", $inputs),
+                $this->hint ? $this->renderHint() : '',
+                $this->renderErrors(),
+            ]);
+        } else {
+            $label = $this->renderLabel();
+            if($this->value) {
+                $this->html['checked'] = 'checked';
+            }
+            $input = strtr($this->template, [
+                '{type}' => $this->type,
+                '{id}' => $this->getHtmlId(),
+                '{name}' => $this->getHtmlName(),
+                '{value}' => 1,
+                '{html}' => $this->getHtmlAttributes()
+            ]);
 
-        $hint = $this->hint ? $this->renderHint() : '';
-        $errors = $this->renderErrors();
-        return implode("\n", [
-            "<input type='hidden' value='' name='" . $this->getHtmlName() . "' />",
-            $input, $label, $hint, $errors
-        ]);
+            $hint = $this->hint ? $this->renderHint() : '';
+            $errors = $this->renderErrors();
+            return implode("\n", [
+                "<input type='hidden' value='' name='" . $this->getHtmlName() . "' />",
+                $input, $label, $hint, $errors
+            ]);
+        }
     }
 }

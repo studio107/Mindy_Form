249c466f8f2daf26a42252cd106f82f625e8603b
Max107 <max@studio107.ru>
2015-07-30 17:18:02 +0300

    Refact AceField

--- src/Mindy/Form/Fields/AceField.php
+++ src/Mindy/Form/Fields/AceField.php
@@ -10,26 +10,27 @@ class AceField extends CharField
 {
     public $template = "<textarea id='{id}' class='hide' name='{name}'{html}>{value}</textarea>";
 
-    public $aceMode = "ace/mode/twig";
+    public $aceMode = "ace/mode/html";
 
     public $aceTheme = "ace/theme/crimson_editor";
 
     public function render()
     {
         $out = strtr('<div id="{id}-ace-editor" class="ace-editor">{value}</div>
-        <script src="https://nightwing.github.io/emmet-core/emmet.js"></script>
         <script type="text/javascript">
             var editor = ace.edit("{id}-ace-editor");
-            editor.setOptions({
-                enableBasicAutocompletion: true,
-                enableSnippets: true,
-                enableLiveAutocompletion: false,
-                enableEmmet: true
-            });
+
+            // Hide deprecate warning
+            editor.$blockScrolling = Infinity;
+
+            editor.setFontSize(".9rem");
+            editor.getSession().setTabSize(4);
+            editor.setShowPrintMargin(false);
+            editor.setAutoScrollEditorIntoView(true);
+            editor.setOption("enableEmmet", true);
             ' . ($this->aceMode ? 'editor.getSession().setMode("' . $this->aceMode . '");' : '') . '
             ' . ($this->aceTheme ? 'editor.setTheme("' . $this->aceTheme . '");' : '') . '
             editor.getSession().on("change", function(e) {
-                console.log(editor.getSession().getValue());
                 $("#{id}").val(editor.getSession().getValue());
             });
         </script>', [

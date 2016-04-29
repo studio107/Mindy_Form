14dee8e8c05eb6f6ed210645b06bea522a95fde2
Falaleev Maxim <max@studio107.ru>
2016-04-29 16:45:27 +0300

    Refact date fields

--- src/Mindy/Form/Fields/DateField.php
+++ src/Mindy/Form/Fields/DateField.php
@@ -2,16 +2,31 @@
 
 namespace Mindy\Form\Fields;
 
+use Mindy\Helper\JavaScript;
+use Mindy\Helper\JavaScriptExpression;
+
 /**
  * Class DateField
  * @package Mindy\Form
  */
 class DateField extends CharField
 {
+    public $options = [
+        'showTime' => false,
+        'showSeconds' => false,
+        'use24hour' => true,
+        'incrementHourBy' => 1,
+        'incrementMinuteBy' => 1,
+        'incrementSecondBy' => 1
+    ];
+
     public function render()
     {
-        $id = $this->getHtmlId();
-        $js = "<script type='text/javascript'>$('#$id').pickmeup({format  : 'Y-m-d'});</script>";
-        return parent::render() . $js;
+        $out = parent::render();
+        $jsOptions = JavaScript::encode(array_merge($this->options, [
+            'field' => new JavaScriptExpression('document.getElementById("' . $this->getHtmlId() . '")')
+        ]));
+        $js = "<script type='text/javascript'>new Pikaday($jsOptions);</script>";
+        return $out . $js;
     }
 }
--- src/Mindy/Form/Fields/DateTimeField.php
+++ src/Mindy/Form/Fields/DateTimeField.php
@@ -6,6 +6,19 @@ namespace Mindy\Form\Fields;
  * Class DateTimeField
  * @package Mindy\Form
  */
-class DateTimeField extends CharField
+class DateTimeField extends DateField
 {
+    public $options = [
+        'showTime' => true,
+        'showSeconds' => false,
+        'use24hour' => true,
+        'incrementHourBy' => 1,
+        'incrementMinuteBy' => 1,
+        'incrementSecondBy' => 1
+    ];
+
+    public function render()
+    {
+        return parent::render();
+    }
 }
